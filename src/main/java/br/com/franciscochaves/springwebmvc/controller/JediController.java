2f302ed8fa986bc1fe1c6a964b43622c55297f88
Francisco Chaves <francisco.jschaves@gmail.com>
2020-07-07 12:34:30 -0300

    Retorne o Jedi cadastrado

--- src/main/java/br/com/franciscochaves/springwebmvc/controller/JediController.java
+++ src/main/java/br/com/franciscochaves/springwebmvc/controller/JediController.java
@@ -1,14 +1,25 @@
 package br.com.franciscochaves.springwebmvc.controller;
 
+import java.util.List;
+
+import br.com.franciscochaves.springwebmvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JediController {
 
     @GetMapping("/jedi")
-    public String jedi() {
-        return "jedi";
+    public ModelAndView jedi() {
+
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+
+        final Jedi luke = new Jedi("Luke", "Skywlker");
+        modelAndView.addObject("allJedi", List.of(luke));
+
+        return modelAndView;
     }
 
 }
