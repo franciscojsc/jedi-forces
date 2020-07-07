4f2994329d7356d8ec0720ba6f516cb915a537a1
Francisco Chaves <francisco.jschaves@gmail.com>
2020-07-07 16:24:05 -0300

    Adicione validação no cadastro

--- src/main/java/br/com/franciscochaves/springwebmvc/controller/JediController.java
+++ src/main/java/br/com/franciscochaves/springwebmvc/controller/JediController.java
@@ -1,15 +1,18 @@
 package br.com.franciscochaves.springwebmvc.controller;
 
-import java.util.List;
 
 import br.com.franciscochaves.springwebmvc.model.Jedi;
 import br.com.franciscochaves.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import javax.validation.Valid;
 
 @Controller
 public class JediController {
@@ -40,9 +43,14 @@ public class JediController {
     }
 
     @PostMapping("/jedi")
-    public String createJedi(@ModelAttribute Jedi jedi) {
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirect) {
+
+        if (result.hasErrors()) {
+            return "new-jedi";
+        }
 
         repository.add(jedi);
+        redirect.addFlashAttribute("message", "Jedi successfully created.");
 
         return "redirect:jedi";
     }
--- src/main/java/br/com/franciscochaves/springwebmvc/model/Jedi.java
+++ src/main/java/br/com/franciscochaves/springwebmvc/model/Jedi.java
@@ -1,8 +1,16 @@
 package br.com.franciscochaves.springwebmvc.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Jedi {
 
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 20, message = "Name must have between 3 and 20 letters")
     private String name;
+
+    @NotBlank(message = "Last Name cannot be blank")
+    @Size(max = 20, message = "Last Name must not have more than 20 letters")
     private String lastName;
 
     public Jedi() {}
